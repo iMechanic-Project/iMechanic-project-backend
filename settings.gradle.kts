87794607fba3d4b5a8deb1790b8140db397849a5
Guillermo Sanchez Palacios <nothicc04@gmail.com>
2024-04-23 00:52:51 -0500

    project-setup

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "iMechanic-Project-Backend"
---
+++ src/main/java/com/imechanic/backend/project/IMechanicProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.imechanic.backend.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IMechanicProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IMechanicProjectBackendApplication.class, args);
+	}
+
+}
